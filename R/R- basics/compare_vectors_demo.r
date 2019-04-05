3e6c43e4f9474e4d53f3cbf6b3bbee464bad65c2
Paul Hughes <paul.hughes@hughesxploration.com>
2019-04-05 17:33:43 -0500

    Compare Vectors Demo

---
+++ R/R- basics/compare_vectors_demo.r
@@ -0,0 +1,14 @@
+#Compare vectors demo
+#Created by Paul Hughes 4/5/19
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+
+# Popular days
+linkedin > 15
+
+# Quiet days
+linkedin <= 5
+
+# LinkedIn more popular than Facebook
+linkedin > facebook
