be31b85ef66358265f3270e3d4c2acddde768f3d
Paul Hughes <paul.hughes@hughesxploration.com>
2019-04-05 16:59:35 -0500

    Greater and less than Demo uploaded

---
+++ R/greater_and_less_than.r
@@ -0,0 +1,11 @@
+#Greater and less than Demo 
+#Created by Paul Hughes 4/5/2019
+
+# Comparison of numerics
+-6 * 5 + 2 >= -10 +1
+
+# Comparison of character strings
+"raining" <= "raining dogs"
+
+# Comparison of logicals
+TRUE > FALSE
